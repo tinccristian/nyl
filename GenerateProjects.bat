6f5a89484106f5fedd68792c3a31c92f1210f56f
tinccristian <50789211+tinccristian@users.noreply.github.com>
2024-01-07 11:40:41 +0200

    Convert build system to premake

---
+++ GenerateProjects.bat
@@ -0,0 +1,2 @@
+call thirdparty\bin\premake\premake5.exe vs2022
+PAUSE
--- Nyl/source/nyl_window.cpp
+++ Nyl/source/nyl_window.cpp
@@ -1,33 +1,33 @@
-#include "nyl_window.h"
-
-NylWindow::NylWindow(int width, int height, const std::string& title)
-    : window(nullptr), width(width), height(height), title(title) {}
-
-NylWindow::~NylWindow() {
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
-
-bool NylWindow::init() {
-    if (!glfwInit()) {
-        return false;
-    }
-
-    window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
-    if (!window) {
-        glfwTerminate();
-        return false;
-    }
-
-    glfwMakeContextCurrent(window);
-    return true;
-}
-
-void NylWindow::update() {
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-}
-
-bool NylWindow::shouldClose() const {
-    return glfwWindowShouldClose(window);
-}
+//#include "nyl_window.h"
+//
+//NylWindow::NylWindow(int width, int height, const std::string& title)
+//    : window(nullptr), width(width), height(height), title(title) {}
+//
+//NylWindow::~NylWindow() {
+//    glfwDestroyWindow(window);
+//    glfwTerminate();
+//}
+//
+//bool NylWindow::init() {
+//    if (!glfwInit()) {
+//        return false;
+//    }
+//
+//    window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+//    if (!window) {
+//        glfwTerminate();
+//        return false;
+//    }
+//
+//    glfwMakeContextCurrent(window);
+//    return true;
+//}
+//
+//void NylWindow::update() {
+//    glfwSwapBuffers(window);
+//    glfwPollEvents();
+//}
+//
+//bool NylWindow::shouldClose() const {
+//    return glfwWindowShouldClose(window);
+//}
